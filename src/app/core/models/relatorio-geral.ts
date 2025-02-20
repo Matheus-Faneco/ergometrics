d7eeb3e1e9516b4809b561a487b4825370485334
faneco <matheusfaneco2009@hotmail.com>
2025-02-20 11:25:49 -0400

    alteraçoes no relatorio geral e estilizaçao

---
+++ src/app/core/models/relatorio-geral.ts
@@ -0,0 +1,5 @@
+export class RelatorioGeral {
+  total_alertas: number = 0;
+  media_alerta_por_funcionario: number = 0;
+  porcentagem_funcionario: number = 0;
+}
---
+++ src/app/core/services/relatorio-geral.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {RelatorioGeral} from '../models/relatorio-geral';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RelatorioGeralService {
+
+  private apiUrlRelatorioGeral = 'http://localhost:8000/api/relatorio-geral/';
+
+  constructor(private http: HttpClient) { }
+
+  getRelatorioGeral(): Observable<RelatorioGeral> {
+    return this.http.get<RelatorioGeral>(this.apiUrlRelatorioGeral);
+  }
+
+}
