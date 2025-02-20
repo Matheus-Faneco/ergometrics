73cd0d0dbbbea2c07b2fb182d001229d7d391106
Matheus Faneco <matheusfaneco2009@hotmail.com>
2025-02-20 19:40:28 -0400

    alteracoes no model e html relatorio geral

--- src/app/core/models/relatorio-geral.ts
+++ src/app/core/models/relatorio-geral.ts
@@ -1,6 +1,6 @@
 export class RelatorioGeral {
   id!: number;
-  total_alertas: number = 3;
-  media_alerta_por_funcionario: number = 4;
-  porcentagem_funcionario: number = 5;
+  total_alertas_geral: number = 0;
+  media_alerta_por_funcionario: number = 0;
+  media_segundos_por_funcionario: number = 0;
 }
